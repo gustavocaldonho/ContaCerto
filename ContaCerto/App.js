49d40ae07b8d263f31df50bf9c9ab068ba87acbe
Gustavo Caldonho <gcaldonho3@gmail.com>
2024-11-11 15:08:52 -0300

    disposição elementos tela principal

--- ContaCerto/App.js
+++ ContaCerto/App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Text, View, SafeAreaView, TouchableOpacity } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Picker } from "@react-native-picker/picker";
+import styles from "./style";
+import BoxHilight from "./src/componentes/BoxHilight";
+import BoxMinusPlus from "./src/componentes/BoxMinusPlus";
 
 export default function App() {
+  const [selectedProduct, setSelectedProduct] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#17B59A" />
+      <Text style={styles.title}>ContaCerto</Text>
+
+      <Picker
+        selectedValue={selectedProduct}
+        onValueChange={(itemValue, itemIndex) => setSelectedProduct(itemValue)}
+        style={styles.picker}
+      >
+        <Picker.Item label="Picolé Simples" value="picolesimples" />
+        <Picker.Item label="Picolé de Cobertura" value="picoledecobertura" />
+        <Picker.Item label="Moreninha" value="moreninha" />
+      </Picker>
+
+      <View style={styles.boxLine}>
+        <BoxHilight value={7} description={"Estoque"} />
+        <BoxMinusPlus value={3} description={"Qtd. Vendida"} />
+      </View>
+      <View style={styles.boxLine}>
+        <BoxHilight value={"1,00"} description={"Custo"} />
+        <BoxHilight value={"1,50"} description={"Valor Final"} />
+      </View>
+      <View style={styles.boxLine}>
+        <BoxHilight value={"0,50"} description={"M. de Lucro"} />
+        <BoxHilight value={"50%"} description={"M. Lucro (%)"} />
+      </View>
+      <View style={styles.boxLine}>
+        <BoxHilight value={"7,25"} description={"Despesa"} />
+        <BoxHilight value={"4,75"} description={"Lucro"} />
+      </View>
+      <View style={styles.boxLine}>
+        <BoxHilight value={"12,00"} description={"Faturamento"} />
+        <TouchableOpacity style={styles.buttonEdit}>
+          <Text style={styles.textEdit}>Editar</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={styles.buttonAdd}>
+        <Text style={styles.textAdd}>Adicionar Produto</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
